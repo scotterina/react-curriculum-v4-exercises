04dc171ae174043572c0adf52c58853b1ac4c2ca
Erin Scott <212621843+scotterina@users.noreply.github.com>
2026-04-09 21:41:08 -0400

    lesson 01

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,11 +2,24 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  let myName = 'Erin';
+  let myAge = 32;
+  const hobbies = ['Drawing', 'Hiking', 'Gardening'];
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+      <p>
+        {' '}
+        Hi there, my name is {myName} and I&apos;m {myAge} years old! I&apos;m
+        from GA (well actually from MD, but that&apos;s neither here nor there).
+        I&apos;m currently learning React with Code the Dream and I&apos;m
+        excited to see what&apos;s to come.
+      </p>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
