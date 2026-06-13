628ebf4fbaef3fe6b8134e82c09fe0b958330428
Erin Scott <212621843+scotterina@users.noreply.github.com>
2026-06-12 21:21:10 -0400

    lesson-10

--- src/exercises/lesson-10/pages/NotFound.jsx
+++ src/exercises/lesson-10/pages/NotFound.jsx
@@ -1,11 +1,13 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router';
 
 export default function NotFound() {
-  const pathname = '';
+  const location = useLocation();
 
   return (
     <section>
       <h2>404: Not Found</h2>
+      <p>The page you are looking for does not exist.</p>
+      <Link to="/">Go Home</Link>
     </section>
   );
 }
